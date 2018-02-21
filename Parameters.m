d4f779a141a6dce61884244957945b8d5ae45c94
Osman <yogurtcuon@gmail.com>
2018-02-21 12:33:47 -0500

    ms0

--- Parameters.m
+++ Parameters.m
@@ -44,7 +44,7 @@ Vec= 0.36920; % L
 kaAg=1e4;%0.28; % day-1
 
 %kel: elimation rate of therapeutic protein (protein-specific)
-kel=6.43;%0.11370; % day-1 6.43;%
+kel=6.43;%0.11370; % day-1
 
 % k12: distribution rate constant from the plasma to the extra central compartment
 k21= 0.0;%10000; % day-1
@@ -236,7 +236,7 @@ AlphaA=8.64E8; % day-1
 BetaA= 0.030130435; % day-1
 
 % BetaC: elimation rate of immune complex
-BetaC=10*BetaA; % day-1
+BetaC=10*kel; % day-1
 
 %% Initial conditions for state variables in the differential equations:
 % AgIS0: initial Ag in the injection site
@@ -252,7 +252,7 @@ Agec0=0; % pmole
 Agp0=0; % pmole
 
 % MS0: Maturation signal (MS), particularly, endotoxin, LPS
-MS0=5*70; % ng ;%Endotoxin;%
+MS0=5*70; % ng Endotoxin;%
 
 % ID0: the initial number of immature dendritc cell
 ID0=5e7; % cells
@@ -406,56 +406,6 @@ pars=pars';
 
 save Parameters.mat;
 
-% function [kon,koff,N,ME0] = doNETMHCIIpan(epitopes,HLA_DR,SimType,NA,numHLADR)
-% %% Collect -on, -off rates, number of epitopes and amount of initial MHC
-% % molecules
-% EpitopePresent = 1;
-% N = length(epitopes);
-% if N<1
-%     EpitopePresent = 0;
-%     epitopes{1} = 'AAAAAAAAAAA'; %Dummy PolyA epitope
-%     N=1;
-% end
-% 
-% if strcmp(HLA_DR{1,1},HLA_DR{1,2}) %homozygot
-%     disp('Homozygote');
-%     ME0=[numHLADR; 0.0;  34E3/2; 34E3/2; 17.1E3/2; 17.1E3/2]/NA*1E12; % pmole
-%     % kon: on rate for for T-epitope-MHC-II binding
-%     kon=EpitopePresent*SimType*repmat([1 0 0 0 0 0],N,1)*8.64*1E-3; %  pM-1day-1
-%     HLAtext = [HLA_DR{1,1} ',' HLA_DR{1,2}];
-% else
-%     ME0=[numHLADR/2; numHLADR/2;  34E3/2; 34E3/2; 17.1E3/2; 17.1E3/2]/NA*1E12; % pmole
-%     % kon: on rate for for T-epitope-MHC-II binding
-%     kon=EpitopePresent*SimType*repmat([1 1 0 0 0 0],N,1)*8.64*1E-3; %  pM-1day-1
-%     HLAtext = [HLA_DR{1,1} ',' HLA_DR{1,2}];
-% end
-% 
-% Affinity_DPQ=[4000 4000 4000 4000]; %place holder for other alleles (NOT USED)
-% Affinity_DR = givekon(epitopes{1},HLAtext);
-% %	koff:	off rate for for T-epitope-MHC-II binding
-% koff=8.64*1E-3*[Affinity_DR Affinity_DPQ]*1E3; %  day-1
-% 
-% for i = 2:N
-%     Affinity_DR = givekon(epitopes{i},HLAtext);
-%     temp = 8.64*1E-3*[Affinity_DR Affinity_DPQ]*1E3;
-%     koff = [koff;temp];
-% end
-% 
-% 
-% function Affinity_DR = givekon(epitopesequence,HLAtext)
-% %% Run NetMHCIIPan if not done before, extract association rates
-% % data(1).Sequence = 'epitopesequence'
-% % data(1).Header = 'Seq'
-% % fastawrite('example.fsa',data);
-% if exist('out.dat')==0 %#ok<EXIST>
-%     dlmwrite('example.fsa',epitopesequence,'')
-%     command = ['netMHCIIpan -f example.fsa -inptype 1 -xls -xlsfile out.dat -a ' HLAtext];
-%     [s,m] = unix(command);
-% end
-% table = readtable('out.dat');
-% Affinity_DR(1) = table{1,'nM'};
-% Affinity_DR(2) = table{1,'nM_1'};
-
 function [kon,koff,N,ME0] = doNETMHCIIpan(epitopes,HLA_DR,SimType,NA,numHLADR)
 %% Collect -on, -off rates, number of epitopes and amount of initial MHC
 % molecules
@@ -469,13 +419,11 @@ end
 
 if strcmp(HLA_DR{1,1},HLA_DR{1,2}) %homozygot
     disp('Homozygote');
-    nallele = 1;
     ME0=[numHLADR; 0.0;  34E3/2; 34E3/2; 17.1E3/2; 17.1E3/2]/NA*1E12; % pmole
     % kon: on rate for for T-epitope-MHC-II binding
     kon=EpitopePresent*SimType*repmat([1 0 0 0 0 0],N,1)*8.64*1E-3; %  pM-1day-1
     HLAtext = [HLA_DR{1,1} ',' HLA_DR{1,2}];
 else
-    nallele = 2;
     ME0=[numHLADR/2; numHLADR/2;  34E3/2; 34E3/2; 17.1E3/2; 17.1E3/2]/NA*1E12; % pmole
     % kon: on rate for for T-epitope-MHC-II binding
     kon=EpitopePresent*SimType*repmat([1 1 0 0 0 0],N,1)*8.64*1E-3; %  pM-1day-1
@@ -483,26 +431,78 @@ else
 end
 
 Affinity_DPQ=[4000 4000 4000 4000]; %place holder for other alleles (NOT USED)
-Affinity_DR = givekon(epitopes{1},nallele,HLA_DR);
+Affinity_DR = givekon(epitopes{1},HLAtext);
 %	koff:	off rate for for T-epitope-MHC-II binding
 koff=8.64*1E-3*[Affinity_DR Affinity_DPQ]*1E3; %  day-1
 
 for i = 2:N
-    Affinity_DR = givekon(epitopes{i},nallele,HLA_DR);
+    Affinity_DR = givekon(epitopes{i},HLAtext);
     temp = 8.64*1E-3*[Affinity_DR Affinity_DPQ]*1E3;
     koff = [koff;temp];
 end
 
-function Affinity_DR = givekon(epitopesequence,nallele,HLAtext)
+
+function Affinity_DR = givekon(epitopesequence,HLAtext)
 %% Run NetMHCIIPan if not done before, extract association rates
 % data(1).Sequence = 'epitopesequence'
 % data(1).Header = 'Seq'
 % fastawrite('example.fsa',data);
-if exist('IEDB_KD.dat')==0 %#ok<EXIST>
-%     dlmwrite('example.fsa',epitopesequence,'')
-%     command = ['netMHCIIpan -f example.fsa -inptype 1 -xls -xlsfile out.dat -a ' HLAtext];
-%     [s,m] = unix(command);
-    IEDBScraper(epitopesequence,nallele,HLAtext);
+if exist('out.dat')==0 %#ok<EXIST>
+    dlmwrite('example.fsa',epitopesequence,'')
+    command = ['netMHCIIpan -f example.fsa -inptype 1 -xls -xlsfile out.dat -a ' HLAtext];
+    [s,m] = unix(command);
 end
-Affinity_DR = dlmread('IEDB_KD.dat');
-Affinity_DR = Affinity_DR(:)';
+table = readtable('out.dat');
+Affinity_DR(1) = table{1,'nM'};
+Affinity_DR(2) = table{1,'nM_1'};
+
+% function [kon,koff,N,ME0] = doNETMHCIIpan(epitopes,HLA_DR,SimType,NA,numHLADR)
+% %% Collect -on, -off rates, number of epitopes and amount of initial MHC
+% % molecules
+% EpitopePresent = 1;
+% N = length(epitopes);
+% if N<1
+%     EpitopePresent = 0;
+%     epitopes{1} = 'AAAAAAAAAAA'; %Dummy PolyA epitope
+%     N=1;
+% end
+% 
+% if strcmp(HLA_DR{1,1},HLA_DR{1,2}) %homozygot
+%     disp('Homozygote');
+%     nallele = 1;
+%     ME0=[numHLADR; 0.0;  34E3/2; 34E3/2; 17.1E3/2; 17.1E3/2]/NA*1E12; % pmole
+%     % kon: on rate for for T-epitope-MHC-II binding
+%     kon=EpitopePresent*SimType*repmat([1 0 0 0 0 0],N,1)*8.64*1E-3; %  pM-1day-1
+%     HLAtext = [HLA_DR{1,1} ',' HLA_DR{1,2}];
+% else
+%     nallele = 2;
+%     ME0=[numHLADR/2; numHLADR/2;  34E3/2; 34E3/2; 17.1E3/2; 17.1E3/2]/NA*1E12; % pmole
+%     % kon: on rate for for T-epitope-MHC-II binding
+%     kon=EpitopePresent*SimType*repmat([1 1 0 0 0 0],N,1)*8.64*1E-3; %  pM-1day-1
+%     HLAtext = [HLA_DR{1,1} ',' HLA_DR{1,2}];
+% end
+% 
+% Affinity_DPQ=[4000 4000 4000 4000]; %place holder for other alleles (NOT USED)
+% Affinity_DR = givekon(epitopes{1},nallele,HLA_DR);
+% %	koff:	off rate for for T-epitope-MHC-II binding
+% koff=8.64*1E-3*[Affinity_DR Affinity_DPQ]*1E3; %  day-1
+% 
+% for i = 2:N
+%     Affinity_DR = givekon(epitopes{i},nallele,HLA_DR);
+%     temp = 8.64*1E-3*[Affinity_DR Affinity_DPQ]*1E3;
+%     koff = [koff;temp];
+% end
+% 
+% function Affinity_DR = givekon(epitopesequence,nallele,HLAtext)
+% %% Run NetMHCIIPan if not done before, extract association rates
+% % data(1).Sequence = 'epitopesequence'
+% % data(1).Header = 'Seq'
+% % fastawrite('example.fsa',data);
+% if exist('IEDB_KD.dat')==0 %#ok<EXIST>
+% %     dlmwrite('example.fsa',epitopesequence,'')
+% %     command = ['netMHCIIpan -f example.fsa -inptype 1 -xls -xlsfile out.dat -a ' HLAtext];
+% %     [s,m] = unix(command);
+%     IEDBScraper(epitopesequence,nallele,HLAtext);
+% end
+% Affinity_DR = dlmread('IEDB_KD.dat');
+% Affinity_DR = Affinity_DR(:)';
